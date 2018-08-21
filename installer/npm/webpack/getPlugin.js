3d86eba5984c5375aa7c7b9acbde6de5c01ddcf2
fts3232 <550610606@qq.com>
2018-08-21 09:27:26 +0800

    v
    v

--- installer/npm/webpack/getPlugin.js
+++ installer/npm/webpack/getPlugin.js
@@ -11,60 +11,57 @@ import { ReactLoadablePlugin } from 'react-loadable/webpack';
 
 let getPlugin = options => {
     let plugin = [];
-    //打包css成1个文件
-    let miniCssExtract = new MiniCssExtractPlugin({
-        // Options similar to the same options in webpackOptions.output
-        // both options are optional
-        filename: 'css/[name].css?v=[contenthash]',
-        //chunkFilename: "css/[id].css?v=[contenthash]"
-    });
-
-    //当开启 HMR 的时候使用该插件会显示模块的相对路径，建议用于开发环境。
-    let namedModules = new webpack.NamedModulesPlugin();
-
-    //生成html文件
-    let html = new HtmlWebpackPlugin({
-        title: 'My App',
-        filename: 'index.html',
-        meta: {
-            description: 'description',
-            keyword: 'keyword',
-        },
-        template: options.templatePath + '/index.html',
-    });
 
     //定义常量
     let define = new webpack.DefinePlugin({
         IS_SERVER_RENDER: options.isServerRender,
     });
-
-    //react-loadable
-    let reactLoadable = new ReactLoadablePlugin({
-        filename: options.buildPath + '/server/react-loadable.json',
-    });
-
     plugin.push(define);
 
     if (!options.isServer) {
         //客户端
         if (options.isServerRender) {
             //服务器渲染
+            //react-loadable
+            let reactLoadable = new ReactLoadablePlugin({
+                filename: options.buildPath + '/server/react-loadable.json',
+            });
             plugin.push(reactLoadable);
         }
+        //生成html文件
+        let html = new HtmlWebpackPlugin({
+            title: 'My App',
+            filename: 'index.html',
+            meta: {
+                description: 'description',
+                keyword: 'keyword',
+            },
+            template: options.templatePath + '/index.html',
+        });
         plugin.push(html);
+        //打包css成1个文件
+        let miniCssExtract = new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'css/[name].css?v=[contenthash]',
+            //chunkFilename: "css/[id].css?v=[contenthash]"
+        });
         plugin.push(miniCssExtract);
     }
+
+    //当开启 HMR 的时候使用该插件会显示模块的相对路径，建议用于开发环境。
     if (options.mode == 'development') {
+        let namedModules = new webpack.NamedModulesPlugin();
         plugin.push(namedModules);
     }
 
     //预渲染
-    const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
-    let prerender = new PrerenderSPAPlugin({
-        staticDir: options.buildPath,
-        routes: ['/', '/Todo', '/Counter'],
-    });
     if (options.prerender) {
+        const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+        let prerender = new PrerenderSPAPlugin({
+            staticDir: options.buildPath,
+            routes: ['/', '/Todo', '/Counter'],
+        });
         plugin.push(prerender);
     }
 
